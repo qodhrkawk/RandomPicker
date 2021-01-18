2b43a51078febd383eda437dd4c8487fb605d194
qodhrkawk <gabriel1120@naver.com>
2021-01-19 02:09:51 +0900

    프로젝트 생성

---
+++ RandomPicker/RandomPicker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPicker
+//
+//  Created by Yunjae Kim on 2021/01/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
